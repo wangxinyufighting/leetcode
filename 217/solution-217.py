9c1ce4c2ed06618e6f607cb64d4e68d447abdaa1
wangxinyu <wxy.782226002@qq.com>
2019-01-18 17:16:42 +0800

    Update solution-217.py

--- 217/solution-217.py
+++ 217/solution-217.py
@@ -1,3 +1,12 @@
+'''
+leetcode显示耗时：52ms
+已经战胜 99.74 % 的 python3 提交记录
+
+思路：
+    利用set函数，将nums变成集合，即去除nums中重复的元素。
+    对比处理前后的nums，若长度相等则没有重复元素，返回false；否则，返回true。
+'''
+
 class Solution:
     def containsDuplicate(self, nums):
         """
