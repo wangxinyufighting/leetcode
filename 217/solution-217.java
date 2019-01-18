f87229c16d4f89e8742daca2aa89eaaeabbe2757
wangxinyu <wxy.782226002@qq.com>
2019-01-18 18:13:19 +0800

    Update solution-217.java

--- 217/solution-217.java
+++ 217/solution-217.java
@@ -1,10 +1,15 @@
 /**
-ss
-ss
-ss
-
+耗时：25ms，击败了27.72% 的用户
+思路1：
+        利用set。遍历nums元素，若set中不含有该元素，则加入set；如含有，则有重复，返回true。
+       
+耗时：7 ms, 击败了83.93% 的用户
+思路2：
+        先排序。
+        遍历数组，若nums[i]和nums[i+1]相等，则返回true。
+        否则最后返回false。
 **/
-public boolean containsDuplicate(int[] nums) {
+public boolean containsDuplicate1(int[] nums) {
         if(nums.length < 2)
             return false;
         Set<Integer> set = new HashSet<>();
@@ -15,7 +20,7 @@ public boolean containsDuplicate(int[] nums) {
         }
         return false;
 }
-public boolean containsDuplicate1(int[] nums){
+public boolean containsDuplicate2(int[] nums){
         if(nums.length < 2)
             return false;
         Arrays.sort(nums);
