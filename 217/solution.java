e959b20d7021732aa88eb078819c8b22ccfbb202
wangxinyu <wxy.782226002@qq.com>
2019-01-18 17:06:19 +0800

    Create solution.java

---
+++ 217/solution.java
@@ -0,0 +1,21 @@
+public boolean containsDuplicate(int[] nums) {
+        if(nums.length < 2)
+            return false;
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(set.contains(nums[i]))
+                return true;
+            set.add(nums[i]);
+        }
+        return false;
+    }
+    public boolean containsDuplicate1(int[] nums){
+        if(nums.length < 2)
+            return false;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-1; i++){
+            if(nums[i] == nums[i+1])
+                return true;
+        }
+        return false;
+    }
